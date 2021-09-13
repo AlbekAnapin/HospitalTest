a6c82fc0dc6a86def71d6771c6525e31ffb262c8
Alibek Anapin <aanapin@gmail.com>
2021-09-13 14:34:46 +0600

    все начинается заново

--- src/test/java/AuthorizationTests.java
+++ src/test/java/AuthorizationTests.java
@@ -6,6 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class AuthorizationTests {
+    WebDriver driver = new ChromeDriver();
+
+        @Test
+    public void Authorization () {
+        driver.get("https://uat.dmed.kz");
+        driver.findElement(By.id("tbUserName")).sendKeys("kmis_octo_726");
+        driver.findElement(By.id("tbPassword")).sendKeys("Asdsadas123@");
+        WebElement login = driver.findElement(By.xpath("//button[text()= 'Войти']"));
+        login.click();
+
+        WaitUntil.WaitElement(driver, By.xpath("//li[@class='auth-service']"));
+        driver.findElement(By.xpath("//li[@class='auth-service']")).click();
+
+    }
 
 
 }
